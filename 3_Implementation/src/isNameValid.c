2ea8e98b5f3a3b123d2f8988c841090c10fb406f
amulyasingaraju <86353200+amulyasingaraju@users.noreply.github.com>
2021-07-14 00:26:49 +0530

    Create isNameValid.c

---
+++ 3_Implementation/src/isNameValid.c
@@ -0,0 +1,18 @@
+#include "library_header.h"
+
+int isNameValid(const char *name) //Check whether name entered is valid (alphabets only)
+{
+    int validName = 1; 
+    int len = 0;
+    int index = 0;
+    len = strlen(name);
+    for(index =0; index <len ; ++index)
+    {
+        if(!(isalpha(name[index])) && (name[index] != '\n') && (name[index] != ' '))
+        {
+            validName = 0;
+            break;
+        }
+    }
+    return validName;
+}
